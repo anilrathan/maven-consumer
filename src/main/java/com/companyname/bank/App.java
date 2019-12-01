861c5eab1133a45f17e5035f1bb0d302cdd37797
anilrathan <57257751+anilrathan@users.noreply.github.com>
2019-12-01 11:46:30 +0530

    adding new line

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World consumer banking Modifed!" );
+        system.out.println( "new file modiefi!"  );
     }
 }
