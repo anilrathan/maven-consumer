d30c9080d02b46a6cfa3a386f6b0fd1ba42e76a6
anilrathan <57257751+anilrathan@users.noreply.github.com>
2019-12-01 11:40:15 +0530

    modifiy

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World consumer banking!" );
+        System.out.println( "Hello World consumer banking Modifed!" );
     }
 }
